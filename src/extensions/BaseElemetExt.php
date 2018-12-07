eab50e27e5306e15b2cdc2cb61042747f1546d44
Buckles <doghusky1990@gmail.com>
2018-12-06 22:48:02 -0400

    added the ability for all elements to have a background colour

--- src/extensions/BaseElemetExt.php
+++ src/extensions/BaseElemetExt.php
@@ -3,12 +3,40 @@
 namespace BucklesHusky\ElementalBase\Extensions;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
+use Heyday\ColorPalette\Fields\ColorPaletteField;
+use SilverStripe\Forms\CheckboxField;
 
 class BaseElemetExt extends DataExtension {
     
     private static $controller_template = 'ElementHolder';
     
-    public function updateCMSFields(\SilverStripe\Forms\FieldList $fields) {
-        $fields = parent::updateCMSFields($fields);
+    private static $db= [
+        'BackgroundColour' => 'Text',
+        'EnableBackgroundColour' => 'Boolean'
+    ];
+    
+    public function updateCMSFields(FieldList $fields) {
+        
+        $fields->addFieldToTab('Root.Settings', CheckboxField::create('EnableBackgroundColour','Enable Background Colour?'));
+        
+        $fields->addFieldToTab(
+            'Root.Settings',
+            ColorPaletteField::create(
+                'BackgroundColour',
+                'Background Colour',
+                array(
+                    'White' => '#fff',
+                    'Black' => '#000',
+                    'Orange' => '#ff6c00',
+                    'Green' => '#83c909',
+                    'Blue' => '#43adff',
+                    'Pink' => '#ff5fa3',
+                    'LightOrange' => '#fc9e38',
+                    'OffBlue' => '#00b3c1',
+                    'Purple' => '#ff276e'
+                )
+            )
+        );
+        
     }
 }
--- templates/DNADesign/Elemental/Layout/ElementHolder.ss
+++ templates/DNADesign/Elemental/Layout/ElementHolder.ss
@@ -1,3 +1,3 @@
-<div class="element test $SimpleClassName.LowerCase<% if $StyleVariant %> $StyleVariant<% end_if %><% if $ExtraClass %> $ExtraClass<% end_if %>" id="$Anchor">
+<div class="element test $SimpleClassName.LowerCase<% if $StyleVariant %> $StyleVariant<% end_if %><% if $ExtraClass %> $ExtraClass<% end_if %>" id="$Anchor" Style="<% if $EnableBackgroundColour %>background-color:$BackgroundColour<% end_if %>">
 	$Element
 </div>
