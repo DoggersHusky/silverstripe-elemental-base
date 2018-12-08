b26317a3070956bee35e90bc27c6643a11d999c8
Buckles <doghusky1990@gmail.com>
2018-12-07 22:03:31 -0400

    added more options

--- src/extensions/BaseElemetExt.php
+++ src/extensions/BaseElemetExt.php
@@ -5,6 +5,9 @@ use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 use Heyday\ColorPalette\Fields\ColorPaletteField;
 use SilverStripe\Forms\CheckboxField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\DropdownField;
 
 class BaseElemetExt extends DataExtension {
     
@@ -12,12 +15,32 @@ class BaseElemetExt extends DataExtension {
     
     private static $db= [
         'BackgroundColour' => 'Text',
-        'EnableBackgroundColour' => 'Boolean'
+        'EnableBackgroundColour' => 'Boolean',
+        'BackgroundPosition'=>'Enum("left top,left center,left bottom,right top,right center,right bottom,center top,center center,center bottom","left top")',
+        'BackgroundParalax'=>'Boolean',
+        'MarginTop' => 'Boolean',
+        'MarginBottom' => 'Boolean',
+        'AddBorderBottom' => 'Boolean',
+        'BorderBottomColour' => 'Text',
+    ];
+    
+    private static $has_one = [
+        'BackgroundImage' => Image::class
+    ];
+    
+    private static $owns = [
+        'BackgroundImage'
     ];
     
     public function updateCMSFields(FieldList $fields) {
         
         $fields->addFieldToTab('Root.Settings', CheckboxField::create('EnableBackgroundColour','Enable Background Colour?'));
+        
+        $fields->addFieldsToTab('Root.Settings', array(
+                CheckboxField::create('MarginTop', 'Add margin to the top of this element?'),
+                CheckboxField::create('MarginBottom', 'Add margin to the bottom of this element?')      
+            )
+        );
         
         $fields->addFieldToTab(
             'Root.Settings',
@@ -35,6 +58,38 @@ class BaseElemetExt extends DataExtension {
                     'OffBlue' => '#00b3c1',
                     'Purple' => '#ff276e'
                 )
+            )
+        );
+        
+        $fields->addFieldToTab('Root.Settings', 
+            UploadField::create('BackgroundImage','Background Image')
+        );
+        
+        $fields->addFieldToTab('Root.Settings', 
+            DropdownField::create('BackgroundPosition', 'Background Position', $this->owner->dbObject('BackgroundPosition')->enumValues(),'left top')
+        );
+        
+        $fields->addFieldToTab('Root.Settings', 
+            CheckboxField::create('BackgroundParalax','Turn background paralax on?')
+        );
+        
+        $fields->addFieldsToTab('Root.Settings', array(
+                CheckboxField::create('AddBorderBottom', 'Add a border to the bottom of this element?'),
+                ColorPaletteField::create(
+                    'BorderBottomColour',
+                    'Bottom Border Colour',
+                    array(
+                        'White' => '#fff',
+                        'Black' => '#000',
+                        'Orange' => '#ff6c00',
+                        'Green' => '#83c909',
+                        'Blue' => '#43adff',
+                        'Pink' => '#ff5fa3',
+                        'LightOrange' => '#fc9e38',
+                        'OffBlue' => '#00b3c1',
+                        'Purple' => '#ff276e'
+                    )
+                )     
             )
         );
         
--- templates/DNADesign/Elemental/Layout/ElementHolder.ss
+++ templates/DNADesign/Elemental/Layout/ElementHolder.ss
@@ -1,3 +1,6 @@
-<div class="element test $SimpleClassName.LowerCase<% if $StyleVariant %> $StyleVariant<% end_if %><% if $ExtraClass %> $ExtraClass<% end_if %>" id="$Anchor" Style="<% if $EnableBackgroundColour %>background-color:$BackgroundColour<% end_if %>">
-	$Element
-</div>
+<section class="element $SimpleClassName.LowerCase<% if $StyleVariant %> $StyleVariant<% end_if %><% if $ExtraClass %> $ExtraClass<% end_if %>" id="$Anchor" Style="<% if $EnableBackgroundColour %>background-color:$BackgroundColour;<% end_if %><% if $BackgroundImage %>background-image:url($BackgroundImage.URL);background-repeat: no-repeat;background-size: cover;<% end_if %>background-position: $BackgroundPosition;<% if $BackgroundParalax %>background-attachment: fixed;<% end_if %><% if $MarginTop %>margin-top: 35px;<% end_if %><% if $MarginBottom %>margin-bottom: 35px;<% end_if %><% if $AddBorderBottom %>border-bottom-color:$BorderBottomColour;border-bottom-style:solid;border-bottom-width:1px;<% end_if %> ">
+    <div class="container">
+        $Element
+    </div>
+</section>
+<% require css("buckleshusky/silverstripe-elemental-base: css/baseElement.css") %>
